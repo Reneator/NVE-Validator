78782eb4a13a27a54e0623fc8676605f146e5cac
Ronney <reneator@googlemail.com>
2020-04-24 13:00:13 +0200

    Finalising godot prototype

--- App.gd
+++ App.gd
@@ -6,7 +6,7 @@ export (PackedScene) var nve_entity_scene
 
 var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
-
+# NVE in this script is always without check_digit
 func validate_nve(nve: String, check_digit: String):
 	clear_error_text()
 	var gen_check_digit
@@ -19,6 +19,9 @@ func validate_nve(nve: String, check_digit: String):
 		return
 	if not check_number(nve):
 		print_error_text("Die NVE darf nur Nummern enthalten!")
+		return
+	if not nve.length() == 19:
+		print_error_text("Die Gesamtlänge der NVE ohne Prüfsumme muss 19 betragen. Aktuell: " + str(nve.length()))
 		return
 	if check_digit:
 		if not check_number(check_digit):
@@ -36,7 +39,6 @@ func validate_nve(nve: String, check_digit: String):
 	else:
 		create_nve_entity(nve, check_digit)
 	
-	pass
 
 func check_number(nve):
 	for chara in nve:
@@ -47,12 +49,32 @@ func check_number(nve):
 	
 func check_check_digit(nve, check_digit): #checks if the given check-digit is correct
 	var generated_check_digit = calculate_check_digit(nve)
-	return int(check_digit) != generated_check_digit
+	return int(check_digit) == generated_check_digit
 
 func calculate_check_digit(nve: String):
 	var number_string = nve.substr(2) #removing the 00 
+	var position = 1
+	var sum = 0
 	for chara in number_string:
+		position += 1
 		var number : int = int(chara)
+		if is_even(position):
+			sum += number*3
+		else:
+			sum += number*1
+	var check_digit_opposed = sum % 10
+	var check_digit
+	
+	if check_digit_opposed == 0:
+		check_digit = 0
+	else:
+		check_digit = 10 - check_digit_opposed
+		
+	return check_digit
+	
+func is_even(number):
+	var mod = number % 2
+	return mod == 0
 
 func create_nve_entity(nve, check_digit):
 	var string = str(nve) + str(check_digit)
