750493c9655d00a8a772cd2112e3ece175687a4d
Ronney <reneator@googlemail.com>
2020-04-25 09:25:45 +0200

    Added java spring boot foundation

---
+++ java/src/main/java/rene/weiss/NVEAPP/NveAppApplication.java
@@ -0,0 +1,13 @@
+package rene.weiss.NVEAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NveAppApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/rene/weiss/NVEAPP/NveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rene.weiss.NVEAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
